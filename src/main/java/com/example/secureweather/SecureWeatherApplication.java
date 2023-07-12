5390f7dad5e435191b35ea45bf4617ea035d5a72
Bukhari bin mahmoud Kibuka <93518580+bumahkib7@users.noreply.github.com>
2023-07-12 12:27:26 +0200

    Initial commit

---
+++ src/main/java/com/example/secureweather/SecureWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.secureweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/secureweather/SecureWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secureweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
