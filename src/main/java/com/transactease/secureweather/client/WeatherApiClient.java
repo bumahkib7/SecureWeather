de4aa14b3872ca823247f7963bbc5b2ce340d5ea
Bukhari bin mahmoud Kibuka <93518580+bumahkib7@users.noreply.github.com>
2023-07-13 16:48:26 +0200

    :fire: repositories

--- src/main/java/com/transactease/secureweather/client/WeatherApiClient.java
+++ src/main/java/com/transactease/secureweather/client/WeatherApiClient.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.util.Date;
 
 @Service
@@ -19,60 +20,28 @@ public class WeatherApiClient {
     @Value("${weatherapi.apikey}")
     private String apiKey;
 
+    @Value("${x-rapid.api.host}")
+    private String apiHost;
+
+    @Value("${x-rapid.baseUrl}")
+    private String baseUrl;
+
     public WeatherApiClient(WebClient.Builder webClient) {
-        this.webClient = webClient.baseUrl("http://api.weatherstack.com").build();
+        this.webClient = webClient.baseUrl("https://ai-weather-by-meteosource.p.rapidapi.com/").build();
     }
 
-    public Mono<City> getCurrentWeatherOfACity(City city) {
-        return webClient.get()
-            .uri(uriBuilder -> uriBuilder.path("/current")
-                .queryParam("access_key", apiKey)
-                .queryParam("query", city.getName())
-                .build()
-            )
-            .retrieve()
-            .onStatus(HttpStatusCode::is4xxClientError, response -> {
-                log.error("4xx error occurred while calling the API.");
-                return Mono.error(new RuntimeException("4xx error occurred while calling the API."));
-            })
-            .onStatus(HttpStatusCode::is5xxServerError, response -> {
-                log.error("5xx error occurred while calling the API.");
-                return Mono.error(new RuntimeException("5xx error occurred while calling the API."));
-            })
-            .bodyToMono(City.class)
-            .onErrorResume(e -> {
-                    log.error("Error occurred: ", e);
-                    return Mono.empty();
-                }
-            )
-            .cache()
-            .retry(3);
-    }
 
-    public Mono<City> getHistoricalWeatherOfSpecificCity(City city, Date date) {
+
+    public Mono<String> getPlaceInfo(String text) {
         return webClient.get()
-            .uri(uriBuilder -> uriBuilder
-                .path("/historical")
-                .queryParam("access_key", apiKey)
-                .queryParam("query", city.getName())
-                .queryParam("historical_date", date)
+            .uri(uriBuilder -> uriBuilder.path("/find_places")
+                .queryParam("text", text)
+                .queryParam("language", "en")
                 .build())
+            .header("X-RapidAPI-Key", apiKey)
+            .header("X-RapidAPI-Host", apiHost)
             .retrieve()
-            .onStatus(HttpStatusCode::is4xxClientError, response -> {
-                log.error("4xx error occurred while calling the API.");
-                return Mono.error(new RuntimeException("4xx error occurred while calling the API."));
-            })
-            .onStatus(HttpStatusCode::is5xxServerError, response -> {
-                log.error("5xx error occurred while calling the API.");
-                return Mono.error(new RuntimeException("5xx error occurred while calling the API."));
-            })
-            .bodyToMono(City.class)
-            .cache()
-            .onErrorResume(e -> {
-                log.error("Error occurred: ", e);
-                return Mono.empty();
-            })
-            .retry(3);
+            .bodyToMono(String.class);
     }
 
 }
---
+++ src/main/java/com/transactease/secureweather/controller/WeatherController.java
@@ -0,0 +1,36 @@
+package com.transactease.secureweather.controller;
+
+import com.transactease.secureweather.client.WeatherApiClient;
+import com.transactease.secureweather.model.City;
+import com.transactease.secureweather.repository.CityRepository;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/weather")
+public class WeatherController {
+
+    private final WeatherApiClient weatherApiClient;
+    private final CityRepository cityRepository;
+
+    public WeatherController(WeatherApiClient weatherApiClient,
+                             CityRepository cityRepository) {
+        this.weatherApiClient = weatherApiClient;
+        this.cityRepository = cityRepository;
+    }
+
+
+    @GetMapping("place/{place}")
+    @Cacheable("City")
+    public Mono<String> getPlaceInfo(@PathVariable String place) {
+        City city = new City();
+        city.setName(place);
+        return weatherApiClient.getPlaceInfo(city.getName()).cache();
+    }
+
+
+}
